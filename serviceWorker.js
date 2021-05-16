97f515b1f895abe42dfd9010da945da8eca5a28b
Jeffrey Cardona <noelitonoelito@gmail.com>
2021-05-16 13:54:54 -0400

    Fixes caching issue with streamable media (MP3)

--- serviceWorker.js
+++ serviceWorker.js
@@ -3,7 +3,7 @@
 const APP_PREFIX = "jumper"
 // version of the off-line cache.
 //   change this value every time you want to update cache.
-const VERSION = "v0.21.0"
+const VERSION = "v0.22.0"
 const CACHE_NAME = `${APP_PREFIX}_${VERSION}`
 const URLS_TO_CACHE = [
   // use "/" instead of "/index.html"
@@ -41,6 +41,8 @@ const URLS_TO_CACHE = [
   `/${APP_PREFIX}/sounds/jump-4.mp3`
 ]
 
+const PARTIAL_CONTENT = 206
+
 function cacheResources(event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -66,20 +68,63 @@ function deleteExpiredCache(event) {
   )
 }
 
+// https://samdutton.github.io/samples/service-worker/prefetch-video/
 function fromCacheElseFetch(event) {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        console.log("responding with cache:", event.request.url)
-        return cachedResponse
-      }
+  console.log("Handling fetch event for", event.request.url)
 
-      console.log("file is not cached, fetching:", event.request.url)
-      return fetch(event.request)
+  if (event.request.headers.get("range")) {
+    const startPosition =
+      Number(/^bytes\=(\d+)\-$/g.exec(event.request.headers.get("range"))[1])
+    console.log("Range request for", event.request.url,
+      ", starting position:", startPosition)
+    event.respondWith(
+      caches.open(CACHE_NAME)
+        .then((cache) => cache.match(event.request.url))
+        .then(async (res) => {
+          if (res) { return res.arrayBuffer() }
+          return fetch(event.request).then(res => res.arrayBuffer())
+        })
+        .then((arrayBuffer) => {
+          return new Response(
+            arrayBuffer.slice(startPosition),
+            {
+              status: PARTIAL_CONTENT,
+              statusText: "Partial Content",
+              headers: [
+                ["Content-Range", "bytes " + startPosition + "-" +
+                  (arrayBuffer.byteLength - 1) + "/" + arrayBuffer.byteLength]]
+            }
+          )
+        })
+    )
+  } else {
+    console.log("Non-range request for", event.request.url)
+    event.respondWith(
+      // caches.match() will look for a cache entry in all of the caches
+      //   available to the service worker.
+      // It's an alternative to first opening a specific named cache and
+      //   then matching on that.
+      caches.match(event.request).then((response) => {
+        if (response) {
+          console.log("Found response in cache:", response)
+          return response
+        }
 
-      // return request || fetch(e.request)
-    })
-  )
+        console.log("No response found in cache. About to fetch from network...")
+
+        return fetch(event.request)
+          .then((response) => {
+            console.log("Response from network is:", response)
+            return response
+          })
+          .catch((error) => {
+            // 404 errors will NOT trigger an exception.
+            console.error("Fetching failed:", error)
+            throw error
+          })
+      })
+    )
+  }
 }
 
 self.addEventListener("install", cacheResources)
